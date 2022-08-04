07ca363954b5008eff49a37e7289d00df97fcf91
lucasbrandao <brandslucas@gmail.com>
2022-08-04 16:21:29 -0300

    implementa signin

--- shortly-back/src/utilities/schemas.js
+++ shortly-back/src/utilities/schemas.js
@@ -2,15 +2,15 @@ import Joi from "joi";
 
 const signupSchema = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    passwordConfirmation: Joi.any().valid(Joi.ref('password'))
+    email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+    passwordConfirmation: Joi.any().valid(Joi.ref('password')).required()
 });
 
 const signinSchema = Joi.object(
     {
-      email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
-      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+      email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).required(),
+      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
     });
 
 export { signupSchema, signinSchema };
