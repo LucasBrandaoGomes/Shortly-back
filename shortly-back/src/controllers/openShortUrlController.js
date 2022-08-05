418d5738bc99673ebf226ac89341366a6780c570
lucasbrandao <brandslucas@gmail.com>
2022-08-05 11:39:33 -0300

    fix incremento de views

--- shortly-back/src/controllers/openShortUrlController.js
+++ shortly-back/src/controllers/openShortUrlController.js
@@ -4,9 +4,11 @@ export async function OpenShortUrl(req, res){
     const { shortUrl } = req.params
 
     try{
-        const { rows } = await connection.query(`SELECT url, short_url FROM urls WHERE short_url ILIKE $1`, [shortUrl]);
+        const { rows } = await connection.query(`SELECT url, short_url, views FROM urls WHERE short_url ILIKE $1`, [shortUrl]);
         if (rows[0]){
             const url = rows[0].url
+            let newValue=rows[0].views +1;
+            await connection.query(`UPDATE urls SET views=$1 WHERE short_url ILIKE $2;`, [newValue, shortUrl])
             res.redirect(`${url}`)
         }else{
             res.sendStatus(404)
