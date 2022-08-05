318460e12b2ee8cc461a850aea87f197a7956fdd
lucasbrandao <brandslucas@gmail.com>
2022-08-05 10:33:05 -0300

    remodela banco de dados

--- database-shortly/scripts/001-cria-tabelas.sql
+++ database-shortly/scripts/001-cria-tabelas.sql
@@ -1,34 +1,32 @@
-CREATE TABLE users_urls (
-	"id" serial NOT NULL,
-	"user_id" integer NOT NULL,
-	"url_shortened_id" integer NOT NULL,
-	"views" integer NOT NULL DEFAULT '0',
-	"create_at" TIMESTAMP NOT NULL DEFAULT 'now ()',
-	CONSTRAINT "users_urls_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE TABLE urls (
-	"id" serial NOT NULL,
-	"url" varchar NOT NULL,
-	"short_url" varchar NOT NULL,
-	"created_at" TIMESTAMP NOT NULL DEFAULT 'now ()',
-	CONSTRAINT "urls_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
 CREATE TABLE users (
 	"id" serial NOT NULL,
 	"name" varchar(50) NOT NULL,
 	"email" varchar(50) NOT NULL UNIQUE,
 	"password" varchar NOT NULL,
-	"created_at" TIMESTAMP NOT NULL DEFAULT 'now ()',
+	"created_at" TIMESTAMP NOT NULL DEFAULT 'now()',
 	CONSTRAINT "users_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
-ALTER TABLE "users_urls" ADD CONSTRAINT "users_urls_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-ALTER TABLE "users_urls" ADD CONSTRAINT "users_urls_fk1" FOREIGN KEY ("url_shortened_id") REFERENCES "urls"("id");
+
+
+CREATE TABLE urls (
+	"id" serial NOT NULL,
+	"user_id" serial NOT NULL,
+	"url" varchar NOT NULL,
+	"short_url" varchar NOT NULL,
+	"created_at" TIMESTAMP NOT NULL DEFAULT 'now()',
+	"views" integer DEFAULT '0',
+	CONSTRAINT "urls_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "urls" ADD CONSTRAINT "urls_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+
+
